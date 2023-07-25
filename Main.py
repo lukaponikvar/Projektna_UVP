d067e82ff8199ed25e36a51c5f5ee199bc94d833
Luka Ponikvar <luka.ponikvar1@gmail.com>
2023-07-25 11:53:15 +0200

    Dodana funkcija za shranjevanje podatkov

---
+++ Main.py
@@ -0,0 +1,6 @@
+# Pridobimo najprej naše podatke
+
+def shrani_v_txt_datoteko(ime_txt_datoteke, html):
+    with open(ime_txt_datoteke, "w", encoding="utf8") as f:
+        f.write(html)
+    return None
