9fb4896dc105733f237b693b2e88d90050e5f66c
Luka Ponikvar <luka.ponikvar1@gmail.com>
2023-07-26 15:15:06 +0200

    Dodani opisi funkcij

--- Pomozne/izluscevanje_podatkov.py
+++ Pomozne/izluscevanje_podatkov.py
@@ -6,6 +6,7 @@ prvotni_url_naslov = "https://math.stackexchange.com/questions"
 
 
 def izlusci_stevilo(niz):
+    """Funkcija iz niza oblike "1,234,567" izlušči število"""
     stevilo = ""
     for i in niz:
         if i != ",":
@@ -13,7 +14,8 @@ def izlusci_stevilo(niz):
     return int(stevilo)
 
 
-def izlusci_stevilo_vseh_strani_in_vprasanj():
+def izlusci_stevilo_vseh_vprasanj_in_vseh_strani():
+    """Funkcija vrne nabor števila vseh vprašanj in števila vseh strani"""
     vzorec = requests.get(prvotni_url_naslov)
     vzorec_za_re = r"""<div class="fs-body3 flex--item fl1 mr12 sm:mr0 sm:mb12">.+?(?P<Stevilo_objav>\d+(,\d+)*).+?questions.+?</div>"""
     stevilo = re.search(vzorec_za_re, vzorec.text, flags=re.DOTALL)
@@ -21,28 +23,30 @@ def izlusci_stevilo_vseh_strani_in_vprasanj():
     return (stevilo.group(1), strani)
 
 
-def izlusci_bloke(tekst):
-    model = r"""<div id="question-summary-\d+" class="s-post-summary.*?</span></time>.*?</div>.*?</div>"""
-    rezultat = re.findall(model, tekst, flags=re.DOTALL)
+def izlusci_bloke(niz):
+    """Funkcija iz HTML datoteke izlušči blok z vsemi informacijami o nekem vprašanju s foruma"""
+    vzorec = r"""<div id="question-summary-\d+" class="s-post-summary.*?</span></time>.*?</div>.*?</div>"""
+    rezultat = re.findall(vzorec, niz, flags=re.DOTALL)
     return rezultat
 
 
-def izlusci_tags(neobdelan_tag):
+def izlusci_oznake(neobdelan_tag):
+    """Funkcija iz niza, ki vsebuje oznake naredi seznam oznak"""
     tags = neobdelan_tag.strip().split(" ")
     return [tag[2:] for tag in tags]
 
 
-def izlusci_podatke1(blok):
+def izlusci_podatke_iz_bloka(blok):
+    """Funkcija iz bloka izlusci vse pomembne informacije o vprašanju in ga vrne kot slovar"""
     vprasanje = {}
     vzorec = re.compile(
         r"""<div id="question-summary-(?P<id>\d+)" class="s-post-summary.*?"""
         r"""<span class="s-post-summary--stats-item-number">(?P<votes>.+?)</span>.*?<span class="s-post-summary--stats-item-unit">.*?</span>.*?<div class="s-post-summary--stats-item( has-answers)?(?P<sprejet> has-accepted-answer)?.*?<span class="s-post-summary--stats-item-number">(?P<answers>.+?)</span>.*?<span class="s-post-summary--stats-item-unit">.*?</span>.*?<span class="s-post-summary--stats-item-number">(?P<views>.+?)</span>.*?<span class="s-post-summary--stats-item-unit">.*?</span>.*?"""
         r"""<h3 class="s-post-summary--content-title">.*?<a href="/questions/\d*?/.*?" class="s-link">(?P<ime>.*?)(?P<duplikat> \[duplicate\])?(?P<zaprto> \[closed\])?</a>.*?"""
         r"""<div class="s-post-summary--meta">.*?<div class="s-post-summary--meta-tags d-inline-block tags js-tags (?P<tag>(t-.*?)*?)">.*?"""
-        r"""<time class="s-user-card--time">asked <span title='(?P<dat>.*?)Z?' class='relativetime'>.*?</span></time>""",
+        r"""<time class="s-user-card--time">asked <span title='(?P<date>.*?)Z?' class='relativetime'>.*?</span></time>""",
         re.DOTALL
     )
-
     najdba = vzorec.search(blok)
     vprasanje["Id"] = najdba["id"]
     vprasanje["Ime"] = najdba["ime"].strip()
@@ -52,14 +56,14 @@ def izlusci_podatke1(blok):
         vprasanje["Opomba"] = "Zaprt"
     else:
         vprasanje["Opomba"] = "Odprt"
-    vprasanje["Oznake"] = izlusci_tags(najdba["tag"])
+    vprasanje["Oznake"] = izlusci_oznake(najdba["tag"])
     vprasanje["Glasovi"] = najdba["votes"].strip()
     vprasanje["Odgovori"] = najdba["answers"].strip()
     vprasanje["Ogledi"] = najdba["views"].strip()
-    vprasanje["Leto"] = najdba["dat"].strip().split(" ")[0].split("-")[0]
-    vprasanje["Mesec"] = najdba["dat"].strip().split(" ")[0].split("-")[1]
-    vprasanje["Dan"] = najdba["dat"].strip().split(" ")[0].split("-")[2]
-    vprasanje["Ura"] = najdba["dat"].strip().split(" ")[1]
+    vprasanje["Leto"] = najdba["date"].strip().split(" ")[0].split("-")[0]
+    vprasanje["Mesec"] = najdba["date"].strip().split(" ")[0].split("-")[1]
+    vprasanje["Dan"] = najdba["date"].strip().split(" ")[0].split("-")[2]
+    vprasanje["Ura"] = najdba["date"].strip().split(" ")[1]
     if najdba["sprejet"]:
         vprasanje["Sprejet odgovor"] = "Da"
     else:
--- Pomozne/shranjevanje_podatkov.py
+++ Pomozne/shranjevanje_podatkov.py
@@ -1,26 +1,31 @@
 import requests
 import csv
-from Pomozne.izluscevanje_podatkov import izlusci_bloke, izlusci_podatke1, izlusci_stevilo_vseh_strani_in_vprasanj
+from izluscevanje_podatkov import izlusci_bloke, izlusci_podatke_iz_bloka, izlusci_stevilo_vseh_vprasanj_in_vseh_strani
+
+najvec_strani = izlusci_stevilo_vseh_vprasanj_in_vseh_strani()[1]
 
 
 def shrani_v_txt_datoteko(ime_txt_datoteke, html):
+    """Funkcija HTML izbrane strani shrani v datoteko z želenim imenom"""
     with open(ime_txt_datoteke, "w", encoding="utf8") as f:
         f.write(html)
     return None
 
 
 def shrani_vprasanja_v_seznam(stran):
+    """Funkcija iz spletne strani potegne HTML in vprašanja na njem shrani v seznam"""
     vprasanja = []
-    html1 = requests.get(stran)
-    bloki = izlusci_bloke(html1.text)
+    html_strani = requests.get(stran).text
+    bloki = izlusci_bloke(html_strani)
     for blok in bloki:
-        vprasanje = izlusci_podatke1(blok)
+        vprasanje = izlusci_podatke_iz_bloka(blok)
         vprasanja.append(vprasanje)
     return vprasanja
 
 
-def shrani_vprasanja(ime_dat):
-    with open(ime_dat, "w", encoding="utf8") as dat:
+def naredi_csv(ime_datoteke):
+    """Funkcija naredi CSV datoteko in vanjo vpiše začetne podatke"""
+    with open(ime_datoteke, "w", encoding="utf8") as dat:
         pisatelj = csv.writer(dat)
         pisatelj.writerow(
             [
@@ -38,29 +43,40 @@ def shrani_vprasanja(ime_dat):
                 "Ura",
             ]
         )
-    strani = izlusci_stevilo_vseh_strani_in_vprasanj()[1]
-    for stevilka_strani in range(1, strani+1):
-        vprasanja = shrani_vprasanja_v_seznam(
-            f"https://math.stackexchange.com/questions?tab=newest&page={stevilka_strani}&pagesize=50"
-        )
-        with open(ime_dat, "a", encoding="utf8") as datt:
-            pisatelj = csv.writer(datt)
-            for vprasanje in vprasanja:
-                pisatelj.writerow(
-                    [
-                        vprasanje["Id"],
-                        vprasanje["Ime"],
-                        vprasanje["Opomba"],
-                        vprasanje["Oznake"],
-                        vprasanje["Glasovi"],
-                        vprasanje["Odgovori"],
-                        vprasanje["Sprejet odgovor"],
-                        vprasanje["Ogledi"],
-                        vprasanje["Leto"],
-                        vprasanje["Mesec"],
-                        vprasanje["Dan"],
-                        vprasanje["Ura"],
-                    ]
-                )
-        print(f"Shranjeno ({stevilka_strani}/{strani})")
-    print("CSV je uspešno shranjen.")
+
+
+def shrani_vprasanja(ime_datoteke, stevilo_strani=najvec_strani):
+    """Funkcija v CSV datoteko z želenim imenom shrani informacije o vprašanjih.Lahko ji predpišemo koliko 
+    strani s foruma želimo, če tega ne storimo, bo funkcija prensesla vse strani. Če delovanje funcije 
+    prekinemo bo vseeno naredila CSV datoteko z do takrat prenesenimi stranmi"""
+    try:
+        if stevilo_strani > najvec_strani:
+            raise ValueError
+        naredi_csv(ime_datoteke)
+        for stevilka_strani in range(1, stevilo_strani+1):
+            vprasanja = shrani_vprasanja_v_seznam(
+                f"https://math.stackexchange.com/questions?tab=newest&page={stevilka_strani}&pagesize=50"
+            )
+            with open(ime_datoteke, "a", encoding="utf8") as datt:
+                pisatelj = csv.writer(datt)
+                for vprasanje in vprasanja:
+                    pisatelj.writerow(
+                        [
+                            vprasanje["Id"],
+                            vprasanje["Ime"],
+                            vprasanje["Opomba"],
+                            vprasanje["Oznake"],
+                            vprasanje["Glasovi"],
+                            vprasanje["Odgovori"],
+                            vprasanje["Sprejet odgovor"],
+                            vprasanje["Ogledi"],
+                            vprasanje["Leto"],
+                            vprasanje["Mesec"],
+                            vprasanje["Dan"],
+                            vprasanje["Ura"],
+                        ]
+                    )
+            print(f"Shranjeno ({stevilka_strani}/{stevilo_strani})")
+        print("CSV je bil uspešno shranjen.")
+    except ValueError:
+        print(f"Vseh strani je le {najvec_strani}!")
