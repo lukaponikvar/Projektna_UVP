5e5c14af99ceb0b464c19e4d428249471c90c6b8
Luka Ponikvar <luka.ponikvar1@gmail.com>
2023-08-04 22:43:06 +0200

    olepšane funkcije

--- Izluscevanje_podatkov.py
+++ Izluscevanje_podatkov.py
@@ -15,7 +15,8 @@ def izlusci_stevilo(niz):
 def izlusci_stevilo_vseh_vprasanj_in_vseh_strani():
     """Funkcija vrne nabor števila vseh vprašanj in števila vseh strani."""
     vzorec = requests.get(f"https://math.stackexchange.com/questions?")
-    vzorec_za_re = r"""<div class="fs-body3 flex--item fl1 mr12 sm:mr0 sm:mb12">.+?(?P<Stevilo_objav>\d+(,\d+)*).+?questions.+?</div>"""
+    vzorec_za_re = (r"""<div class="fs-body3 flex--item fl1 mr12 sm:mr0 sm:mb12">.+?"""
+                    r"""(?P<Stevilo_objav>\d+(,\d+)*).+?questions.+?</div>""")
     stevilo = re.search(vzorec_za_re, vzorec.text, flags=re.DOTALL)
     strani = math.ceil(izlusci_stevilo(stevilo.group(1)) / 50)
     return (stevilo.group(1), strani)
@@ -26,9 +27,6 @@ def izlusci_bloke(niz):
     vzorec = r"""<div id="question-summary-\d+" class="s-post-summary.*?(?:(?:>Community wiki)|(?:</span></time>))"""
     rezultat = re.findall(vzorec, niz, flags=re.DOTALL,)
     return rezultat
-
-# <div id="question-summary-\d+" class="s-post-summary.*?(?:(?:>Community wiki)|(?:</span></time>))
-# <div id="question-summary-\d+" class="s-post-summary.*?(?:</span></time>)?(?:>Community wiki)?</div>
 
 
 def izlusci_oznake(neobdelan_tag):
@@ -42,10 +40,16 @@ def izlusci_podatke_iz_bloka(blok):
     vprasanje = {}
     vzorec = re.compile(
         r"""<div id="question-summary-(?P<id>\d+)" class="s-post-summary.*?"""
-        r"""<span class="s-post-summary--stats-item-number">(?P<votes>.+?)</span>.*?<span class="s-post-summary--stats-item-unit">.*?</span>.*?<div class="s-post-summary--stats-item( has-answers)?(?P<sprejet> has-accepted-answer)?.*?<span class="s-post-summary--stats-item-number">(?P<answers>.+?)</span>.*?<span class="s-post-summary--stats-item-unit">.*?</span>.*?<div class="s-post-summary--stats-item.*?" title="(?P<views>\d+) views?">.*?"""
-        r"""<h3 class="s-post-summary--content-title">.*?<a href="/questions/\d*?/.*?" class="s-link">(?P<ime>.*?)(?P<duplikat> \[duplicate\])?(?P<zaprto> \[closed\])?</a>.*?"""
-        r"""<div class="s-post-summary--meta">.*?<div class="s-post-summary--meta-tags d-inline-block tags js-tags (?P<tag>(t-.*?)*?)">.*?"""
-        r"""((<time class="s-user-card--time">asked <span title='(?P<date>.*?)Z?' class='relativetime'>.*?</span></time>)|(Community wiki))""",
+        r"""<span class="s-post-summary--stats-item-number">(?P<votes>.+?)</span>.*?<span class="s-post-summary-"""
+        r"""-stats-item-unit">.*?</span>.*?<div class="s-post-summary--stats-item( has-answers)?(?P<sprejet> has-accepted-answer)"""
+        r"""?.*?<span class="s-post-summary--stats-item-number">(?P<answers>.+?)</span>.*?<span class="s-post-summary--stats-"""
+        r"""item-unit">.*?</span>.*?<div class="s-post-summary--stats-item.*?" title="(?P<views>\d+) views?">.*?"""
+        r"""<h3 class="s-post-summary--content-title">.*?<a href="/questions/\d*?/.*?" class="s-link">(?P<ime>.*?)"""
+        r"""(?P<duplikat> \[duplicate\])?(?P<zaprto> \[closed\])?</a>.*?"""
+        r"""<div class="s-post-summary--meta">.*?<div class="s-post-summary--meta-tags d-inline-block tags js-tags """
+        r"""(?P<tag>(t-.*?)*?)">.*?"""
+        r"""((<time class="s-user-card--time">asked <span title='(?P<date>.*?)Z?' class='relativetime'>"""
+        r""".*?</span></time>)|(Community wiki))""",
         re.DOTALL
     )
     najdba = vzorec.search(blok)
